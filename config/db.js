e11735ec3ab1e116d1b5902a0494d60f62ed2588
Bibekksah123 <163726097+Bibekksah123@users.noreply.github.com>
2025-04-29 07:30:50 +0545

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
 
 const connectionToDB=async()=>{
-  await mongoose.connect("mongodb://127.0.0.1:27017/courses")
+  await mongoose.connect("")
 }
 
 module.exports = connectionToDB
